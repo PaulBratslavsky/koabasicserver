f4e4a26cf501953129b6579572f6d8721b3afe26
paul bratslavsky <paulbratslavsky@pauls-MBP.home>
2022-03-04 02:13:08 -0500

    set up mongo db connetion

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
+require("dotenv").config();
+
 const Koa = require("koa");
 const Router = require("koa-router");
+const mongoose = require("mongoose");
 const bodyParser = require("koa-bodyparser");
 const postRoutes = require("./api/post/config/routes.js");
 
-const app = new Koa();
-const router = new Router();
+const url = process.env.MONGO_DB_URI;
 
-app.use(bodyParser());
-app.use(router.routes());
+async function start(url) {
 
+  // database setup
+  try {
+    await mongoose.connect(url);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error.message);
+    return;
+  }
 
-app.use(async (ctx, next) => {
-  console.log(
-    `${ctx.method} ${ctx.url} ${ctx.status} ${new Date().toLocaleString()}`
-  );
-  await next();
-});
+  // create the koa app
+  const app = new Koa();
+  const router = new Router();
 
+  app.use(bodyParser());
+  app.use(router.routes());
 
-// generate routes dynamically
-postRoutes.forEach((route) => {
-  return router[route.method](route.route, async (ctx) =>
-    route.controller[route.type](ctx)
-  );
-});
+  // set up routes
+  postRoutes.forEach((route) => {
+    return router[route.method](route.route, async (ctx) =>
+      route.controller[route.type](ctx)
+    );
+  });
 
-const PORT = 4000;
-app.listen(PORT);
-console.log(`Server is listening on port ${PORT}`);
+  // start the server
+  const PORT = 4000;
+  app.listen(PORT);
+  console.log(`Server is listening on port ${PORT}`);
+}
 
+start(url);
+
+// mongoose.connect(connectionString)
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error('Could not connect to MongoDB', err));
+// mongoose.connection.on('error', console.error);
 
 // router.get("/posts", (ctx) => post.find(ctx));
 // router.get("/posts/:id", (ctx) => post.findOne(ctx));
 // router.post("/posts", (ctx) => post.create(ctx));
 // router.delete("/posts/:id", (ctx) => post.delete(ctx));
 // router.put("/posts/:id", (ctx) => post.update(ctx));
-
 
 // router.get("/comments", (ctx) => {
 //   ctx.body = comments;
